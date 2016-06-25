989aa0f9f5d6a55ccc434d48fe5450ce8fba52aa
Miguel Rodriguez <marbrb1@gmail.com>
2016-06-25 10:56:16 -0500

    def str methods and install pillow

--- biblioApp/models.py
+++ biblioApp/models.py
@@ -8,14 +8,23 @@ class Editor(models.Model):
     pais = models.CharField(max_length=50)
     website = models.URLField()
 
+    def __str__(self):
+        return self.nombre
+
 class Autor(models.Model):
     nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=40)
+    apellidos = models.CharField(max_length=40)
     email = models.EmailField()
+
+    def __str__(self):
+        return "{} {}".format(self.nombre, self.apellidos)
 
 class Libro(models.Model):
     titulo = models.CharField(max_length=100)
     autores = models.ManyToManyField(Autor)
     editor = models.ForeignKey(Editor)   #un solo editor
     fecha_publicacion = models.DateField()
-    portada = models.ImageField(upload_to='portadas')
+    portada = models.ImageField(upload_to='portadas')   #apt-get install libjpeg-dev
+
+    def __str__(self):
+        return self.titulo
