912b9cfdafd68c8d4d8fe2e9142be910893e1d05
root <root@localhost.localdomain>
2016-06-27 17:30:24 -0500

    update models and administration interfaz

--- biblioApp/admin.py
+++ biblioApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from biblioApp.models import Editor, Autor, Libro
 
-# Register your models here.
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'email')
+    search_fields = ('nombre', 'apellidos') #barra de busqueda
+
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'editor', 'fecha_publicacion')
+    list_filter = ('fecha_publicacion',)
+    ordering = ('-fecha_publicacion',)  #Tambien se puede en la clase Meta de Libro
+    
+admin.site.register(Editor)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Libro, LibroAdmin)
--- biblioApp/models.py
+++ biblioApp/models.py
@@ -8,23 +8,31 @@ class Editor(models.Model):
     pais = models.CharField(max_length=50)
     website = models.URLField()
 
+    class Meta: #clase especial para metadatos
+        ordering = ["nombre"]
+        verbose_name_plural = "Editores"
+
     def __str__(self):
         return self.nombre
 
 class Autor(models.Model):
     nombre = models.CharField(max_length=30)
-    apellidos = models.CharField(max_length=40)
-    email = models.EmailField()
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField(blank=True, verbose_name='e-mail')
+
+    class Meta:
+        verbose_name_plural = "Autores"
 
     def __str__(self):
-        return "{} {}".format(self.nombre, self.apellidos)
+        return "{} {}".format(self.nombre, self.apellido)
 
 class Libro(models.Model):
     titulo = models.CharField(max_length=100)
     autores = models.ManyToManyField(Autor)
     editor = models.ForeignKey(Editor)   #un solo editor
-    fecha_publicacion = models.DateField()
-    portada = models.ImageField(upload_to='portadas')   #apt-get install libjpeg-dev
+    fecha_publicacion = models.DateField(blank=True, null=True)
+    portada = models.ImageField(upload_to='portadas', default=None)   #apt-get install libjpeg-dev
+
 
     def __str__(self):
         return self.titulo
