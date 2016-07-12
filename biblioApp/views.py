2c28f0631ba4d95af53f882fe2b42e9f75fc1a9c
miguel <miguel@localhost.localdomain>
2016-07-12 11:48:38 -0500

    added url for HTTP headers

--- biblioApp/views.py
+++ biblioApp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
-# Create your views here.
+def atributos_meta(request):
+    valor = request.META.items()    #return a list of tuples.
+
+    return render_to_response('meta.html', {'path':request.path,'secure':request.is_secure(),'lista':valor})
